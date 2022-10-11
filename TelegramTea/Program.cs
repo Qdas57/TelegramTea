3e4df0df46dc46aff35ff738080a71869358720d
VerbitskiA <eternity.2019@mail.ru>
2022-10-11 21:46:05 +0300

    ДЗ

--- TelegramTea/Program.cs
+++ TelegramTea/Program.cs
@@ -5,6 +5,13 @@ namespace ConsoleApp
 {
     class Program
     {
+        //TODO:
+        //1. Добавить тег к photoEntity
+        //2. Я хочу по тегу получать фото, которое имеет этот тег, если фото несколько - случайную.
+        //3. Добавить логику добавления фото в бд
+        //
+        //Паттерны проектирования: репозиторий.
+
         static void Main(string[] args)
         {
             PhotoContext photoContext = new PhotoContext();
