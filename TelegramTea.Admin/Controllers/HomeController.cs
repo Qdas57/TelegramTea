07d8c28ef19c2b735b86967e8892910dafff8ea7
VerbitskiA <eternity.2019@mail.ru>
2022-10-17 19:08:07 +0300

    Добавил проект админки.

---
+++ TelegramTea.Admin/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramTea.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Photos()
+        {
+            //TODO: достать все фото и вывести на страницу (bootstrap)
+            return View();
+        }
+    }
+}
